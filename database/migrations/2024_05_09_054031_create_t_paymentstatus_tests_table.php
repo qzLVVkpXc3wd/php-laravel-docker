7908148d7571b4851c0bbafcfecf0e3af5533a7f
qzLVVkpXc3wd <168059449+qzLVVkpXc3wd@users.noreply.github.com>
2024-05-09 17:18:10 +0900

    Update 2024_05_09_054031_create_t_paymentstatus_tests_table.php

--- database/migrations/2024_05_09_054031_create_t_paymentstatus_tests_table.php
+++ database/migrations/2024_05_09_054031_create_t_paymentstatus_tests_table.php
@@ -17,18 +17,6 @@ return new class extends Migration
             $table->string('cust_code')->nullable()->comment('顧客 ID');  
             $table->string('order_id')->nullable()->comment('購入 ID');
             $table->string('item_id')->nullable()->comment('商品 ID');
-            $table->string('pay_item_id')->nullable()->comment('外部決済機関商品 ID');
-            $table->string('item_name')->nullable()->comment('商品名称');
-            $table->string('tracking_id')->nullable()->comment('トラッキング ID');
-            $table->string('free1')->nullable()->comment('自由欄１');
-            $table->string('free2')->nullable()->comment('自由欄２');
-            $table->string('free3')->nullable()->comment('自由欄３');
-            $table->string('res_pay_method')->nullable()->comment('処理結果 支払方法');
-            $table->string('res_result')->nullable()->comment('処理結果ステータス');
-            $table->string('res_tracking_id')->nullable()->comment('処理トラッキング ID');
-            $table->string('res_payinfo_key')->nullable()->comment('顧客決済情報');
-            $table->string('res_err_code')->nullable()->comment('エラーコード');
-            $table->string('sps_hashcode')->nullable()->comment('チェックサム');            
             $table->timestamps();
         });
     }
