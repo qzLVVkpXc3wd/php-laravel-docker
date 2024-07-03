5d9dc3f1e29da965f8548a4a436a21eaeb481cbf
qzLVVkpXc3wd <168059449+qzLVVkpXc3wd@users.noreply.github.com>
2024-07-03 09:57:50 +0900

    Update SearchController.php

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,7 +10,7 @@ class SearchController extends Controller
     public function store(Request $request)
     {
         $param = $request->input('param');
-
+        logs()->notice($param);
         $results = T_Syutsugansya::leftJoin('m_nys_sbt','t_syutsugansyas.senbatu_kbn','=','m_nys_sbt.nys_sbt_cd')
             ->where('google_account', $param)->select('t_syutsugansyas.*','m_nys_sbt.nys_sbt_name')->get();
         logs()->notice('test', ['ここ' => 'きた']);
