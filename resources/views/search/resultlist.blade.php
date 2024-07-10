41dfcc6b1238b4f512e807e512c901ecf0100c0f
qzLVVkpXc3wd <168059449+qzLVVkpXc3wd@users.noreply.github.com>
2024-07-10 11:20:46 +0900

    Update resultlist.blade.php

--- resources/views/search/resultlist.blade.php
+++ resources/views/search/resultlist.blade.php
@@ -12,7 +12,7 @@
                 @csrf
                 @foreach ($results as $index => $result)
                     <div class="row">
-                        <button type="submit" name="submit" value="{{ $result->nys_sbt_cd }}" {!! empty($result->gohi_sbt_name) 
+                        <button type="submit" name="submit" value="{{ $result->nys_sbt_cd }}" {!! empty($result->gohi_sbt_name) || $result->gohi_date > NOW()
                             ? "class='btn btn-secondary disabled'"
                             : "class='btn btn-primary'" !!}>
                             {{ $result->nys_sbt_name }}
