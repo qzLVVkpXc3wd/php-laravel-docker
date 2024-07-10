f7430be9301cdd0d6f68e4f2b46521fe1f48363e
qzLVVkpXc3wd <168059449+qzLVVkpXc3wd@users.noreply.github.com>
2024-07-10 14:12:45 +0900

    Update SearchController.php

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -11,7 +11,10 @@ class SearchController extends Controller
     {
         $param = $request->input('param');
         $results = T_Syutsugansya::leftJoin('m_nys_sbt','t_syutsugansyas.senbatu_kbn','=','m_nys_sbt.nys_sbt_cd')
-            ->where('google_account', $param)->select('t_syutsugansyas.*','m_nys_sbt.nys_sbt_name')->get();
+              ->where('google_account', $param)
+              ->orderBy('m_nys_sbt.nys_sbt_cd')              
+              ->select('t_syutsugansyas.*','m_nys_sbt.nys_sbt_name')
+              ->get();
         return view('search.results', ['results' => $results]);
     }
     public function exam(Request $request)
