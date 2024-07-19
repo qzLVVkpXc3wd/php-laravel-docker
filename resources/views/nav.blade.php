1e610f8b800654cbb7c375669a8aa3409d54962d
qzLVVkpXc3wd <168059449+qzLVVkpXc3wd@users.noreply.github.com>
2024-07-19 09:21:00 +0900

    Add files via upload

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,7 @@
+<nav class="navbar">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="/top">
+        <img src="{{asset('/img/nav-brand.png')}}" width="200" height="50" alt="天使大学">
+    </a>
+    </div>
+</nav>
