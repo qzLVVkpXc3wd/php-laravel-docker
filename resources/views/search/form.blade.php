ef5b482fe66ecd519731382f6c610696b6188a7d
qzLVVkpXc3wd <168059449+qzLVVkpXc3wd@users.noreply.github.com>
2024-07-19 16:51:26 +0900

    Update form.blade.php

--- resources/views/search/form.blade.php
+++ resources/views/search/form.blade.php
@@ -1,14 +1,40 @@
-<x-layout>
+<head>
+    @include('includeHead')
+</head>
 
-    <head>
-        <title>検索フォーム</title>
-    </head>
+<body>
+    <div class="container bg-primary-subtle">
+        <h1>
+            検索フォーム
+        </h1>
+    </div>
 
-    <body>
-        <form action="/search" method="POST">
-            @csrf
-            <input type="google_account" name="param" placeholder="検索条件を入力">
-            <button type="submit">検索</button>
-        </form>
-    </body>
-</x-layout>
+    <div class="container">
+        <div class="col-md-8 mx-auto">
+            <div class="row">
+                <label>登録情報の確認</label>
+                <form action="/search" method="POST">
+                    @csrf
+                    <input type="google_account" name="param" placeholder="検索条件を入力">
+                    <button type="submit">POST</button>
+                </form>
+            </div>
+            <div class="row">
+                <label>受験票</label>
+                <form action="/exam" method="POST">
+                    @csrf
+                    <input type="google_account" name="param" placeholder="検索条件を入力">
+                    <button type="submit">POST</button>
+                </form>
+            </div>
+            <div class="row">
+                <label>合否</label>
+                <form action="/examresult" method="POST">
+                    @csrf
+                    <input type="google_account" name="param" placeholder="検索条件を入力">
+                    <button type="submit">POST</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
