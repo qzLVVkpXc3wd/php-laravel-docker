ad5ed4af0fd401919370367cd9fa5b03f07faa38
qzLVVkpXc3wd <168059449+qzLVVkpXc3wd@users.noreply.github.com>
2024-07-22 16:41:23 +0900

    Update examresult.blade.php

--- resources/views/search/examresult.blade.php
+++ resources/views/search/examresult.blade.php
@@ -32,33 +32,35 @@
                                     @switch ($result->gohi_sbt_cd)
                                         @case('01')
                                             <div class="container mt-5 bg-image">
-                                                <div class="row justify-content-center">
-                                                    <div class="col-md-8">
-                                                        <h1 class="text-center">合 格 通 知</h1>
-                                                        <div class="mt-5">
-                                                            <div class="row mb-3 justify-content-end">
-                                                                <div class="col-5 text-right">
-                                                                    {{ '受 験 番 号   ' . $result->juken_cd }}</div>
-                                                            </div>
-                                                            <div class="row mb-3 justify-content-end">
-                                                                <div class="col-5 text-right">
-                                                                    {{ '氏　　　名   ' . $result->shigansya_sei_kanji . '　' . $result->shigansya_mei_kanji }}
+                                                <div class="bg-cover">
+                                                    <div class="row justify-content-center">
+                                                        <div class="col-md-8">
+                                                            <h1 class="text-center">合 格 通 知</h1>
+                                                            <div class="mt-5">
+                                                                <div class="row mb-3 justify-content-end">
+                                                                    <div class="col-5 text-right">
+                                                                        {{ '受 験 番 号   ' . $result->juken_cd }}</div>
+                                                                </div>
+                                                                <div class="row mb-3 justify-content-end">
+                                                                    <div class="col-5 text-right">
+                                                                        {{ '氏　　　名   ' . $result->shigansya_sei_kanji . '　' . $result->shigansya_mei_kanji }}
+                                                                    </div>
                                                                 </div>
                                                             </div>
-                                                        </div>
-                                                        <div class="mt-5 text-right">
-                                                            <p>{{ $result->nyushi_nendo }}年度{{ $result->nys_sbt_name }}の結果、{{ $result->print_gakka_name }}に{{ $result->gohi_sbt_disp_name }}したことを通知いたします。
-                                                            </p>
-                                                        </div>
-                                                        <div class="mt-5">
-                                                            <div class="row mb-3 justify-content-end">
-                                                                <div class="col-8 text-right">
-                                                                    {{ date('Y年m月d日', strtotime($result->gohi_date)) }}
-                                                                </div>
-                                                                <div class="col-4 text-left">天使大学</div>
+                                                            <div class="mt-5 text-right">
+                                                                <p>{{ $result->nyushi_nendo }}年度{{ $result->nys_sbt_name }}の結果、{{ $result->print_gakka_name }}に{{ $result->gohi_sbt_disp_name }}したことを通知いたします。
+                                                                </p>
                                                             </div>
-                                                            <div class="row mb-3 justify-content-end">
-                                                                <div class="col-4 text-left">学長　田畑 邦治</div>
+                                                            <div class="mt-5">
+                                                                <div class="row mb-3 justify-content-end">
+                                                                    <div class="col-8 text-right">
+                                                                        {{ date('Y年m月d日', strtotime($result->gohi_date)) }}
+                                                                    </div>
+                                                                    <div class="col-4 text-left">天使大学</div>
+                                                                </div>
+                                                                <div class="row mb-3 justify-content-end">
+                                                                    <div class="col-4 text-left">学長　田畑 邦治</div>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
@@ -68,33 +70,35 @@
 
                                         @case('02')
                                             <div class="container mt-5 bg-image">
-                                                <div class="row justify-content-center">
-                                                    <div class="col-md-8">
-                                                        <h1 class="text-center">不 合 格 通 知</h1>
-                                                        <div class="mt-5">
-                                                            <div class="row mb-3 justify-content-end">
-                                                                <div class="col-5 text-right">
-                                                                    {{ '受 験 番 号   ' . $result->juken_cd }}</div>
-                                                            </div>
-                                                            <div class="row mb-3 justify-content-end">
-                                                                <div class="col-5 text-right">
-                                                                    {{ '氏　　　名   ' . $result->shigansya_sei_kanji . '　' . $result->shigansya_mei_kanji }}
+                                                <div class="bg-cover">
+                                                    <div class="row justify-content-center">
+                                                        <div class="col-md-8">
+                                                            <h1 class="text-center">不 合 格 通 知</h1>
+                                                            <div class="mt-5">
+                                                                <div class="row mb-3 justify-content-end">
+                                                                    <div class="col-5 text-right">
+                                                                        {{ '受 験 番 号   ' . $result->juken_cd }}</div>
+                                                                </div>
+                                                                <div class="row mb-3 justify-content-end">
+                                                                    <div class="col-5 text-right">
+                                                                        {{ '氏　　　名   ' . $result->shigansya_sei_kanji . '　' . $result->shigansya_mei_kanji }}
+                                                                    </div>
                                                                 </div>
                                                             </div>
-                                                        </div>
-                                                        <div class="mt-5 text-right">
-                                                            <p>{{ $result->nyushi_nendo }}年度{{ $result->nys_sbt_name }}の結果、{{ $result->print_gakka_name }}に{{ $result->gohi_sbt_disp_name }}したことを通知いたします。
-                                                            </p>
-                                                        </div>
-                                                        <div class="mt-5">
-                                                            <div class="row mb-3 justify-content-end">
-                                                                <div class="col-8 text-right">
-                                                                    {{ date('Y年m月d日', strtotime($result->gohi_date)) }}
-                                                                </div>
-                                                                <div class="col-4 text-left">天使大学</div>
+                                                            <div class="mt-5 text-right">
+                                                                <p>{{ $result->nyushi_nendo }}年度{{ $result->nys_sbt_name }}の結果、{{ $result->print_gakka_name }}に{{ $result->gohi_sbt_disp_name }}したことを通知いたします。
+                                                                </p>
                                                             </div>
-                                                            <div class="row mb-3 justify-content-end">
-                                                                <div class="col-4 text-left">学長　田畑 邦治</div>
+                                                            <div class="mt-5">
+                                                                <div class="row mb-3 justify-content-end">
+                                                                    <div class="col-8 text-right">
+                                                                        {{ date('Y年m月d日', strtotime($result->gohi_date)) }}
+                                                                    </div>
+                                                                    <div class="col-4 text-left">天使大学</div>
+                                                                </div>
+                                                                <div class="row mb-3 justify-content-end">
+                                                                    <div class="col-4 text-left">学長　田畑 邦治</div>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
