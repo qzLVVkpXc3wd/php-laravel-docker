88f9c952da05cdb97cbb94988c9113b8051cc5ac
qzLVVkpXc3wd <168059449+qzLVVkpXc3wd@users.noreply.github.com>
2024-07-22 15:42:05 +0900

    Update admission.blade.php

--- resources/views/search/admission.blade.php
+++ resources/views/search/admission.blade.php
@@ -5,9 +5,9 @@
 <body>
     <div class="container">
         @if (empty($results))
-            <h2>
-                該当する結果はありません。
-            </h2>
+            <details>
+                <summary>該当する結果はありません。</summary>
+            </details>
         @else
             <div class="container-fluid">
                 @php
@@ -21,12 +21,12 @@
                                     <h1 class="text-center">入 学 許 可 書</h1>
                                     <div class="mt-5">
                                         <div class="row mb-3 justify-content-end">
-                                            <div class="col-5 text-right">
+                                            <div class="col-12 col-md-5 text-right">
                                                 {{ '受 験 番 号   ' . $result->juken_cd }}
                                             </div>
                                         </div>
                                         <div class="row mb-3 justify-content-end">
-                                            <div class="col-5 text-right">
+                                            <div class="col-12 col-md-5 text-right">
                                                 {{ '氏　　　名   ' . $result->shigansya_sei_kanji . '　' . $result->shigansya_mei_kanji }}
                                             </div>
                                         </div>
@@ -37,15 +37,15 @@
                                     </div>
                                     <div class="mt-5">
                                         <div class="row mb-3 justify-content-end">
-                                            <div class="col-8 text-right">
+                                            <div class="col-12 col-md-8 text-right">
                                                 {{ date('Y年m月d日', strtotime($result->gohi_date)) }}
                                             </div>
-                                            <div class="col-4 text-left">
+                                            <div class="col-12 col-md-4 text-left">
                                                 天使大学
                                             </div>
                                         </div>
                                         <div class="row mb-3 justify-content-end">
-                                            <div class="col-4 text-left">学長　田畑 邦治</div>
+                                            <div class="col-12 col-md-4 text-left">学長　田畑 邦治</div>
                                         </div>
                                     </div>
                                 </div>
@@ -58,11 +58,11 @@
                 @endif
             @endforeach
             @if ($flg == false)
-                <h2>
-                    該当する結果はありません。
-                </h2>
+                <details>
+                    <summary>該当する結果はありません。</summary>
+                </details>
             @endif
-            </div>
-        @endif
-    </div>
+        </div>
+    @endif
+</div>
 </body>
